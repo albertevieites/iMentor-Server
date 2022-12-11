4a3d19c852d5f4579c852aa295e82df8ec149700
Alberte Vieites <albertevieites@gmail.com>
2022-12-11 21:50:05 +0000

    refactor: :fire: refactor

--- routes/questions.routes.js
+++ routes/questions.routes.js
@@ -1,93 +1,86 @@
-const router = require("express").Router()
+const router = require('express').Router();
 
-const { isAuthenticated } = require("../middlewares/jwt.middleware")
-const { OwnerOnly } = require("../middlewares/ownerOnly.middleware")
-const Users = require('../models/User.model')
-const Questions = require('../models/question.model')
-const Comments = require('../models/comment.model')
+const { isAuthenticated } = require('../middlewares/jwt.middleware');
+// const { OwnerOnly } = require('../middlewares/ownerOnly.middleware');
+const Users = require('../models/User.model');
+const Questions = require('../models/question.model');
+const Comments = require('../models/comment.model');
 
-
-router.get("/questions", (req, res) => {
-
- Questions
-    .find().populate('owner')
+router.get('/questions', (req, res) => {
+  Questions.find()
+    .populate('owner')
     .then(questions => res.json(questions))
-    .catch(err => res.status(500).json(err))
-})
+    .catch(err => res.status(500).json(err));
+});
 
-//add question
-router.post("/questions", isAuthenticated, (req, res) => {
-    const { imageUrl, title, description, skills } = req.body
-    const userId = req.payload._id
+// Add question
+router.post('/questions', isAuthenticated, (req, res) => {
+  const { imageUrl, title, description, skills } = req.body;
+  const userId = req.payload._id;
 
-    Questions
-       .create({ imageUrl, owner: userId, title, description, skills  })
-       .then(question=>{
-        Users.findByIdAndUpdate(userId,{$push:{questions:question._id}})
-        .then(()=>res.json(question))
+  Questions.create({ imageUrl, owner: userId, title, description, skills })
+    .then(question => {
+      Users.findByIdAndUpdate(userId, {
+        $push: { questions: question._id },
+      }).then(() => res.json(question));
     })
-       .catch(err => res.status(500).json(err))
-       
-   })
+    .catch(err => res.status(500).json(err));
+});
 
-router.get("/questions/:id",  isAuthenticated, (req, res) => {
-    
-    const {id} = req.params
+router.get('/questions/:id', isAuthenticated, (req, res) => {
+  const { id } = req.params;
 
-    Questions
-       .findById(id).populate('owner')
-       .populate("Comments")
-        .populate({path:"Comments", populate:{path:"user"}})
-       .then(question => res.json(question))
-       .catch(err => res.status(500).json(err))
-   })
+  Questions.findById(id)
+    .populate('owner')
+    .populate('Comments')
+    .populate({ path: 'Comments', populate: { path: 'user' } })
+    .then(question => res.json(question))
+    .catch(err => res.status(500).json(err));
+});
 
-router.post("/questions/:id/delete", isAuthenticated,  (req, res) => {
-    
-    const {id} = req.params
-    const userId = req.payload._id
-    
-    Questions
-       .findByIdAndDelete(id)
-        .then(()=>Users.findByIdAndUpdate(userId, {$pull: {questions: id}}))
-       .catch(err => res.status(500).json(err))
-   })
+router.post('/questions/:id/delete', isAuthenticated, (req, res) => {
+  const { id } = req.params;
+  const userId = req.payload._id;
 
-   router.post("/questions/:id/edit", isAuthenticated,  (req, res) => {
-    
-    const {id} = req.params
-    const { imageUrl, title, description, skills } = req.body
-    
-    Questions
-       .findByIdAndUpdate(id, { imageUrl, title, description, skills  })
-       .catch(err => res.status(500).json(err))
-        
-   })
+  Questions.findByIdAndDelete(id)
+    .then(() => Users.findByIdAndUpdate(userId, { $pull: { questions: id } }))
+    .catch(err => res.status(500).json(err));
+});
 
-router.post("/questions/:id/comment/add", isAuthenticated, (req, res) => {
-    const comment = req.body.comment
-    const questionId = req.params.id
-    const userId = req.payload._id
+router.post('/questions/:id/edit', isAuthenticated, (req, res) => {
+  const { id } = req.params;
+  const { imageUrl, title, description, skills } = req.body;
 
-    Comments
-    .create({ user: userId,text: comment})
-    .then((newComment)=>
-    Questions
-    .findByIdAndUpdate(questionId, {$push: { Comments: newComment._id}},{new: true})
-    .then((updatedQuestion)=> 
-        Users
-        .findByIdAndUpdate(userId, {$push: { Comments: newComment._id}})
-            .then(()=>
-            Questions
-                .findOne(updatedQuestion)
-                .populate("Comments")
-                .populate({path:"Comments", populate:{path:"user"}})
-                )
-            .then((populatedQuestion)=>res.json(populatedQuestion))
-    ))
-    .catch((err)=>console.log(err))
+  Questions.findByIdAndUpdate(id, {
+    imageUrl,
+    title,
+    description,
+    skills,
+  }).catch(err => res.status(500).json(err));
+});
 
-   
-   })
+router.post('/questions/:id/comment/add', isAuthenticated, (req, res) => {
+  const comment = req.body.comment;
+  const questionId = req.params.id;
+  const userId = req.payload._id;
 
-module.exports = router
+  Comments.create({ user: userId, text: comment })
+    .then(newComment =>
+      Questions.findByIdAndUpdate(
+        questionId,
+        { $push: { Comments: newComment._id } },
+        { new: true }
+      ).then(updatedQuestion =>
+        Users.findByIdAndUpdate(userId, { $push: { Comments: newComment._id } })
+          .then(() =>
+            Questions.findOne(updatedQuestion)
+              .populate('Comments')
+              .populate({ path: 'Comments', populate: { path: 'user' } })
+          )
+          .then(populatedQuestion => res.json(populatedQuestion))
+      )
+    )
+    .catch(err => console.log(err));
+});
+
+module.exports = router;
